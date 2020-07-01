6f47bbe5e974b7640467d9076ff0a00c8edb0360
Christopher Murray <cmurray1105@gmail.com>
2020-07-01 07:44:19 -0500

    commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 4201;
+const httpProxy = require('http-proxy');
+
+app.use(express.static(path.join(__dirname, 'frontEnd')));
+app.use(express.json())
+
+const proxy = httpProxy.createProxyServer({});
+
+
+    app.all('/api/getReviews', (req, res)=>{
+    proxy.web(req,res,{target:'http://111111-env.eba-9uquamkj.us-east-2.elasticbeanstalk.com/'})
+  });
+
+    app.all('/products', (req, res)=>{
+    proxy.web(req,res,{target:'http://newcarousel-env.eba-irp2rurw.us-east-2.elasticbeanstalk.com/'})
+    });
+    app.all('/api/get/products', (req, res)=>{
+
+    proxy.web(req,res,{target:'http://searchbarricardo2-dev.us-east-2.elasticbeanstalk.com/'})
+  })
+    app.all('/display', (req, res)=>{
+    proxy.web(req,res,{target:'http://imagecomponent-env-1.eba-4mfwjdhg.us-east-2.elasticbeanstalk.com/'})
+    // res.status(200)
+  })
+
+app.listen(PORT, () => {
+  console.log(`server is CONNECTED on PORT:${PORT}`)
+});
